5ac28a6b7be563fd6d69197f72d74e1c92b90c1e
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-25 18:21:02 -0400

    Update Buttons.jsx

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useAddRowCallback, useSetValueCallback } from 'tinybase/ui-react';
-import Celon from './Celon';
+import { useSetValueCallback } from 'tinybase/ui-react';
 
 // Convenience function for generating a random integer
 const getRandom = (max = 100) => Math.floor(Math.random() * max);
@@ -12,25 +11,12 @@ export const Buttons = ({ onSave }) => {
     () => (value) => value + 1
   );
   const handleRandom = useSetValueCallback('random', () => getRandom());
-  const handleAddPet = useAddRowCallback('pets', (_, store) => ({
-    name: ['fido', 'felix', 'bubbles', 'lowly', 'polly'][getRandom(5)],
-    species: store.getRowIds('species')[getRandom(5)],
-  }));
 
   // Click counter
   const handleClickCount = useSetValueCallback(
     'clickCounter',
     () => (value) => (value || 0) + 1
   );
-
-  const celonRef = React.useRef(null);
-
-  const handleFetchCeloAddress = async () => {
-    if (celonRef.current) {
-      const address = await celonRef.current.getAddress();
-      console.log('Fetched Celo address:', address);
-    }
-  };
 
   const handleAction = (action) => {
     action();
@@ -41,11 +27,8 @@ export const Buttons = ({ onSave }) => {
     <div id='buttons'>
       <button onClick={() => handleAction(handleCount)}>Increment number</button>
       <button onClick={() => handleAction(handleRandom)}>Random number</button>
-      <button onClick={() => handleAction(handleAddPet)}>Add a pet</button>
-      <button onClick={handleFetchCeloAddress}>Fetch Celo Address</button>
       <button onClick={() => handleAction(handleClickCount)}>Count Clicks</button>
       <button onClick={onSave}>Save Data</button>
-      <Celon ref={celonRef} />
     </div>
   );
 };
